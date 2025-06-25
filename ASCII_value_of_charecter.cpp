e86a3002472d9605a8d76cfee0f66efe6a5c80e4
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-26 02:47:32 +0530

    add program to find ASCII value of character

---
+++ ASCII_value_of_charecter.cpp
@@ -0,0 +1,8 @@
+//wrinting a program to print ASCII value of a character(numerical value of the character)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char c='A';//creating a string variable that store A
+    cout<<int(c);//printing the numerical value of A
+    return 0;
+}
