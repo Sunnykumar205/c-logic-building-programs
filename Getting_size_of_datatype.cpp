fd088a2bc2762bfcd9b0161a0b5fc87d1add668b
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-26 03:15:36 +0530

    getting size of datatypes

--- Getting_size_of_datatype.cpp
+++ Getting_size_of_datatype.cpp
@@ -1,12 +1,15 @@
+//program to find the size of the datatype 
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a;
-    float b;
-    char c;
-    double d;
+    int a;//declaring int type datatype 4
+    float b;//declaring float type datatype 4
+    char c;//declaring character type datatype 1
+    double d;//declaring double type datatype 8
+    //to get the size of datatype we use sizeif()function
     cout<<sizeof(a)<<endl;
     cout<<sizeof(b)<<endl;
     cout<<sizeof(c)<<endl;
     cout<<sizeof(d);
+    return 0;
 }
