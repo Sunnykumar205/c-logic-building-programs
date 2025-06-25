2bad586ca9f2f1d62e04e25a2c195d002c7bed24
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-26 02:56:53 +0530

    swap two numbers program

---
+++ swap_two_numbers.cpp
@@ -0,0 +1,12 @@
+//writing program to swap two numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,temp;
+    cin>>a>>b;
+    temp=a;//storing the value of a to temp variable
+    a=b;//storing value of b to a
+    b=temp;//again storing value of temp to b
+    cout<<"a = "<<a<<endl<<"b = "<<b;//printing the output
+    return 0;
+}
