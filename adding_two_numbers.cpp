8b5bc04211f417f153a17701b20f20f45766cf44
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-26 02:20:05 +0530

    adding two numbers program

---
+++ adding_two_numbers.cpp
@@ -0,0 +1,9 @@
+//program to add two numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b,c;//creating variables to hold user input
+    cin>>a>>b;//taking input from user
+    c=a+b;//adding two numbers and store its value in c
+    cout<<c;//printing the value of c
+}
