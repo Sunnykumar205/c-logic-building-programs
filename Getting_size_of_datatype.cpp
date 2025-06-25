afb110c417d828f560f2b20239d8feebbdc5e9be
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-26 03:10:45 +0530

    finding size of data types

---
+++ Getting_size_of_datatype.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    float b;
+    char c;
+    double d;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d);
+}
