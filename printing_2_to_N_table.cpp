6537febf94c4dbfe425c1ad46419e0a8eaedeb37
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-28 15:44:04 +0530

    printing_table_of_N_number

---
+++ printing_2_to_N_table.cpp
@@ -0,0 +1,11 @@
+//program to write the table of number N
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=1;i<=N;i++){//creating a loop that comoare the value of i to N and increase tha value of i
+        cout<<N<<" * "<<i<<" = "<<i*N<<endl;//printing the value of the table of number N
+    }
+    return 0;
+}
