cc3b903d098fa8d37b1dcb721d2a1162bff0e8a1
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-28 15:34:44 +0530

    print_integers_1_to_N

---
+++ print_integers_1_to_N.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=1;i<=N;i++)//creating a loop to compare and increase the value till N
+    cout<<i<<endl;//printing the value of i
+}
