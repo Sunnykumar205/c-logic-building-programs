a9b720fcb90e6091bab6fba3e8f1b8bd03fee576
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-28 16:00:20 +0530

    Reverse_of_any_table

---
+++ Reverse_of_any_table.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int N;
+    cin>>N;
+    for(int i=10;i>=1;i--){
+        cout<<N<<" * "<<i<<" = "<<N*i<<endl;
+    }
+    return 0;
+}
