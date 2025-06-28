2cbd60e6b11b59c76ef47c2a20098b35c111ce84
sunnykumar205 <sunnykumar813011@gmail.com>
2025-06-28 15:50:54 +0530

    printing table of N number to 10 digits

--- printing_2_to_N_table.cpp
+++ printing_2_to_N_table.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int N;
     cin>>N;
-    for(int i=1;i<=N;i++){//creating a loop that comoare the value of i to N and increase tha value of i
+    for(int i=1;i<=10;i++){//creating a loop that comoare the value of i to N and increase tha value of i
         cout<<N<<" * "<<i<<" = "<<i*N<<endl;//printing the value of the table of number N
     }
     return 0;
