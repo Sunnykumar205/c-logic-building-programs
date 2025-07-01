4d705cc8bbeb507d617a13c1c2225a5434696a16
sunnykumar205 <sunnykumar813011@gmail.com>
2025-07-02 01:42:05 +0530

    program to check even or odd detail

--- program_to_check_even_or_odd.cpp
+++ program_to_check_even_or_odd.cpp
@@ -1,9 +1,10 @@
+//program to check the number is even or odd
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int a;
-    cin>>a;
-    if(a%2==0){
+    cin>>a;//taking input from user
+    if(a%2==0){//if else using to check the condition is fully divided by two is even else the number is odd
         cout<<"Even";
     }
     else{
