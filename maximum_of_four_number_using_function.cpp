d2251ab91cd75ebaa90c7bd3b5dc0b039ad16a0e
sunnykumar205 <sunnykumar813011@gmail.com>
2025-07-08 02:08:34 +0530

    program to find maximum of numbers using function

--- maximum_of_four_number_using_function.cpp
+++ maximum_of_four_number_using_function.cpp
@@ -1,14 +1,15 @@
+//program to find maximum of four number by using function
 #include <iostream>
 #include<bits/stdc++.h>
 #include <cstdio>
 using namespace std;
 
-int max_of_four(int a, int b, int c, int d){
-    if(a>b && a>c && a>d){
+int max_of_four(int a, int b, int c, int d){//creating a function to check the maximum of the given numbers
+    if(a>b && a>c && a>d){//first comdition
         return a;
-    }else if(b>a && b>c && b>d){
+    }else if(b>a && b>c && b>d){//second condition
         return b;
-    }else if(c>a && c>b && c>d){
+    }else if(c>a && c>b && c>d){//third condition
         return c;
     }else {
         return d;
@@ -18,7 +19,7 @@ int main() {
     int a, b, c, d;
     cin>>a>>b>>c>>d;
   //  scanf("%d %d %d %d", &a, &b, &c, &d);
-    int ans = max_of_four(a, b, c, d);
+    int ans = max_of_four(a, b, c, d);//passing the value from the function
   //  printf("%d", ans);
   cout<<"maximum of numbers : "<<ans;
     
