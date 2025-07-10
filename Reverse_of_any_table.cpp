bc6483ffc65b4f575ce764f5360b434991ce3280
sunnykumar205 <sunnykumar813011@gmail.com>
2025-07-11 01:21:46 +0530

    Reverse_of_any_table adding details

--- Reverse_of_any_table.cpp
+++ Reverse_of_any_table.cpp
@@ -1,9 +1,10 @@
+//prograt to print the reverse order of table
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
     int N;
     cin>>N;
-    for(int i=10;i>=1;i--){
+    for(int i=10;i>=1;i--){//create a loop to print revese of table
         cout<<N<<" * "<<i<<" = "<<N*i<<endl;
     }
     return 0;
