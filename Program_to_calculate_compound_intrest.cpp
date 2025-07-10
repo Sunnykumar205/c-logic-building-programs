704ad9b0aceaad9778fc5a7ee63654019863f140
sunnykumar205 <sunnykumar813011@gmail.com>
2025-07-11 01:13:29 +0530

    Program_to_calculate_compound_intrest adding details

--- Program_to_calculate_compound_intrest.cpp
+++ Program_to_calculate_compound_intrest.cpp
@@ -1,17 +1,19 @@
+//program to calculate compound intrest
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    double principle,rate,year,A,CI,c=1,n;
+    double principle,rate,year,A,CI,c=1,n;//defining the variables of type double
     cout<<"enter the principle"<<endl;
-    cin>>principle;
+    cin>>principle;//taking principle amount from user
     cout<<"enter rate"<<endl;
-    cin>>rate;
+    cin>>rate;//taking intrest rate from user
     cout<<"enter years"<<endl;
-    cin>>year;
+    cin>>year;//taking years from user
     
     A=(1+rate/100);
+    //  this is predefined function to calculate the power of a value
    // double square=(pow(A,year));
-   for(int i=1;i<=year;i++){
+   for(int i=1;i<=year;i++){//creating a loop to calcualte the power of a function without using pow function
     c=c*A;
    } 
    //another loop for calculation the power of a value without using pow funtion
@@ -19,8 +21,8 @@ int main(){
   do{c=c*A;
  i++;}
   while(i<year);*/
-   n=c*principle;
-   CI=n-principle;
-   cout<<"compound intrest is : "<<CI;
+   n=c*principle;//clculating the amount 
+   CI=n-principle;//calcualtion the compound intrest
+   cout<<"compound intrest is : "<<CI;//printing the compound intrest
     return 0;
 }
