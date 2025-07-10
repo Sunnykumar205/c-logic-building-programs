8d2244d59660790c7a7d54cc2f78a682ba50dc8b
sunnykumar205 <sunnykumar813011@gmail.com>
2025-07-11 01:11:04 +0530

    program_for_printing_value_using_recursion adding details

--- program_for_printing_value_using_recursion.cpp
+++ program_for_printing_value_using_recursion.cpp
@@ -1,17 +1,17 @@
+//program to print numbers given by user using recursion
 #include<bits/stdc++.h>
 using namespace std;
-    int printingnumber(int lrange,int urange){
+    int printingnumber(int lrange,int urange){//creating function for checking the base case and printing numbers by using head recursion
         //base case
-        if(lrange>urange){
+        if(lrange>urange){//deciding base case
         return 0;
         }
         cout<<lrange<<endl;
-        printingnumber(lrange+1,urange);
+        printingnumber(lrange+1,urange);//calling function and increasing the value
 }
 int main(){
     int lrange,urange;
-    cin>>lrange>>urange;
-    printingnumber(lrange,urange);
-   // cout<<result<<endl;
+    cin>>lrange>>urange;//taking input from the user
+    printingnumber(lrange,urange);//passing the value from the function
     return 0;
 }
