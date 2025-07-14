492b20af3028f9bf7b72444395257277c1ba4635
sunnykumar205 <sunnykumar813011@gmail.com>
2025-07-15 01:38:31 +0530

    Program_to_print_reverse_of_a_string full sentence

--- Program_to_print_reverse_of_a_string.cpp
+++ Program_to_print_reverse_of_a_string.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     string sent;//initializing the string 
-  cin>>sent;//taking input from the user
+  getline(cin,sent);//taking input from the user
     int a=sent.size();//this is for showing the size of the string 
     for(int i=a;i>=0;i--){//using loop to reverse the string and print from the last word
         cout<<sent[i];//printing the string 
